43bb53decbdffd94b0f44a1e09e1bd81947f882d
Saikoyomii9 <saikoyomii9@gmail.com>
2023-01-24 10:53:25 -0500

    January 24

---
+++ src/modules/Orders/index.js
@@ -0,0 +1,18 @@
+import { Card, Table, Tag } from "antd";
+import orders from '../../data/data/dashboard/orders.json';
+import { useNavigate } from "react-router-dom";
+
+const Orders = () => {
+        return (
+                <div>Orders</div>
+        );
+};
+
+        const styles = {
+
+                page: {
+                        margin: 20,
+                },
+};
+
+export default Orders;
